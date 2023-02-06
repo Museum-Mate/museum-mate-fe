d43563e95c33b673d38c9205d8d9a492adbb4ff0
Jiyoung <ahn.jiyoung15@gmail.com>
2023-02-06 16:12:07 +0900

    Feat: 전시 함께 가요 슬라이더 구현

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 // exhibitions.html
 async function getExhibitionsById() {
-    let url = 'http://localhost:8080/api/v1/exhibitions?sort=id';
+    let url = 'http://localhost:8080/api/v1/exhibitions?size=10&sort=id';
     try {
         let res = await fetch(url);
         return await res.json();
@@ -15,13 +15,16 @@ async function renderExhibitionsById() {
     let exhibition = exhibitions.result.content;
     let html = '';
     exhibition.forEach(element => {
-        let htmlSegment = `<div class="swiper-slide">
-        <div class="testimonial-item">
-            <img src=${element.mainImgUrl} class="testimonial-img" alt="">
-            <h3>${element.name}</h3>
-            <h4>${element.galleryLocation}</h4>
-        </div>
-    </div><!-- End testimonial item -->`;
+        let htmlSegment = `
+        <div class="swiper-slide">
+            <a href="/work-single">
+                <div class="testimonial-item">
+                    <img src=${element.mainImgUrl} class="testimonial-img" alt="">
+                    <h3>${element.name}</h3>
+                    <h4>${element.galleryLocation}</h4>
+                </div>
+            </a>
+        </div>`;
 
         html += htmlSegment;
     });
@@ -31,7 +34,7 @@ async function renderExhibitionsById() {
 }
 
 async function getExhibitionsByEndAt() {
-    let url = 'http://localhost:8080/api/v1/exhibitions?sort=endAt';
+    let url = 'http://localhost:8080/api/v1/exhibitions?size=10&sort=endAt';
     try {
         let res = await fetch(url);
         return await res.json();
@@ -46,13 +49,16 @@ async function renderExhibitionsByEndAt() {
     let exhibition = exhibitions.result.content;
     let html = '';
     exhibition.forEach(element => {
-        let htmlSegment = `<div class="swiper-slide">
-        <div class="testimonial-item">
-            <img src=${element.mainImgUrl} class="testimonial-img" alt="">
-            <h3>${element.name}</h3>
-            <h4>${element.galleryLocation}</h4>
-        </div>
-    </div><!-- End testimonial item -->`;
+        let htmlSegment = 
+        `<div class="swiper-slide">
+            <a href="/work-single">
+                <div class="testimonial-item">
+                    <img src=${element.mainImgUrl} class="testimonial-img" alt="">
+                    <h3>${element.name}</h3>
+                  <h4>${element.galleryLocation}</h4>
+              </div>
+            </a>
+        </div>`;
 
         html += htmlSegment;
     });
@@ -60,3 +66,51 @@ async function renderExhibitionsByEndAt() {
     let container = document.querySelector('#swiper-wrapper2');
     container.innerHTML = html;
 }
+
+async function getGatheringsById() {
+    let url = 'http://localhost:8080/api/v1/gatherings?size=10';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+        alert("Request Error!");
+    }
+}
+
+async function renderGatheringsById() {
+    let exhibitions = await getGatheringsById();
+    let exhibition = exhibitions.result.content;
+    let html = '';
+    exhibition.forEach(element => {
+        let htmlSegment = 
+        `<div class="swiper-slide">
+        <div class="pricing-horizontal row col-11 m-auto d-flex shadow rounded overflow-hidden bg-white">
+            <div class="pricing-horizontal-icon col-md-3 text-center bg-secondary text-light py-4">
+                <img src=${element.exhibitionMainUrl} class="together-img" alt="">
+            </div>
+            <div
+                class="pricing-horizontal-body offset-lg-1 col-md-6 col-lg-4 d-flex align-items-center pl-5 pt-lg-0 pt-4">
+                <ul class="text-left px-4 list-unstyled mb-0 light-300">
+                    <h2>${element.title}</h2>
+                    <p><i class="bx bx-map"></i>${element.meetLocation}</p><br>
+                    <h5>${element.content}</h5>
+                </ul>
+            </div>
+            <div class="pricing-horizontal-tag col-md-2 text-center pt-3 d-flex align-items-center">
+                <div class="w-100 light-300">
+                    <p><i class="bx bx-group">${element.currentPeople}/${element.maxPeople}</i></p>
+                    <a href="#" class="btn rounded-pill px-4 btn-outline-primary mb-3">참여하기</a>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('#swiper-wrapper3');
+    container.innerHTML = html;
+}
+
+
