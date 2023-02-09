47cbf71d45337e7533cc866fc4c4e271bd3bbe10
leejoonsang <djm04212@gmail.com>
2023-02-10 05:06:32 +0900

    feat: 함께 전시봐요 모집글 작성 페이지 구현

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get("/pricing", (req, res) => {
     res.sendFile(__dirname + "/pricing.html");
 });
 
+app.get("/gathering-new", (req, res) => {
+    // res.sendFile(__dirname + "/gathering-new.html");
+    res.redirect(__dirname + "/work.html");
+});
+
 app.use((req, res) => {
     res.sendFile(__dirname + "/error.html");
 });
@@ -28,3 +33,40 @@ app.listen(3000, (err) => {
     if (err) return console.log(err);
     console.log("The server is listening on port 3000");
 });
+
+/*
+// 전시 상세조회 페이지 -> 함께 전시가요 글 등록
+// 전시 정보 전달을 위한 setting
+
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+app.use('/public', express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// view 세팅 설정 
+app.set('views', path.join(__dirname, './public'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+router.route("/exhibition-single").get((req, res)=>{
+    res.render("exhibition-single.html");
+});
+
+router.route("/gathering-new").post((req, res)=>{
+    res.render("gathering-new.html");
+});
+
+app.use("/", router);
+
+// 넘어온 정보를 받아서 뿌려주기
+// router.route("/gathering-new").post((req, res)=>{
+//     console.log("data immigrated")
+//     var exhibitionInfo = {id:req.body.}
+// })
+
+*/
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,19 @@ const footer = document.querySelector('footer');
 fetch('/footer.html')
 .then(res => res.text())
 .then(data => footer.innerHTML = data);
+
+// fetch header 토큰 부분 수정 필요 
+function postGathering(payload) {
+    const data = {};
+    payload.forEach((value, key) => (data[key] = value));
+    console.log(data);
+
+    fetch("http://localhost:8080/api/v1/gatherings/posts", {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRqbTA0MjEyQGdtYWlsLmNvbSIsImlhdCI6MTY3NTk1NzU5OCwiZXhwIjoxNjc1OTU3ODk4fQ.TbtnLwef3bMHPW98khLcuRPx0pdp4Mp-C07ulqcq2dU",
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => console.log(response));
+}
