f734ce641fa0bce0b056db4ea15bcf2148600f9e
Geuun <jgk9612@gmail.com>
2023-02-12 02:49:26 +0900

    feat: login 페이지 연결
    app.js 에 login 페이지 연결 코드 추가

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-
-    res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/about.html");
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
 });
 
-app.get("/contact", (req, res) => {
-    res.sendFile(__dirname + "/contact.html");
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/contact.html');
 });
 
-app.get("/pricing", (req, res) => {
-    res.sendFile(__dirname + "/pricing.html");
+app.get('/pricing', (req, res) => {
+    res.sendFile(__dirname + '/pricing.html');
 });
 
 app.use((req, res) => {
-    res.sendFile(__dirname + "/error.html");
+    res.sendFile(__dirname + '/error.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/join', (req, res) => {
+    res.sendFile(__dirname + '/join.html');
 });
 
 app.listen(3000, (err) => {
     if (err) return console.log(err);
-    console.log("The server is listening on port 3000");
+    console.log('The server is listening on port 3000');
 });
