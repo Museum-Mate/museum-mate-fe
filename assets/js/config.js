0bcdc9f65d1e13571577ecaa6b6b0a160beac762
hyeonju01 <tangled6648@gmail.com>
2023-02-14 13:10:21 +0900

    Chore: 페이지 리다이렉트를 위해 Base Url 추가

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,5 @@
-// const BASE_URL = 'http://localhost:8080';
-const BASE_URL = 'http://api.withmuma.com';
+const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://api.withmuma.com';
+
+const FRONT_BASE_URL = 'http://localhost:3000';
 const urlSearchParams = new URLSearchParams(location.search);
